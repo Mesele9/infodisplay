912f26ae577c6b908c08c194e3a9aaed56751cc7
Mesele Assefa <getmesy@gmail.com>
2023-09-18 11:47:04 +0000

    install and create Django app

---
+++ infodisplay/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from display import urls
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('display.urls'))
+]
